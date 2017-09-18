f5c07815953c97869a0c46d641c3df376201732c
Alexandre Di Guida <alexandre.diguida@gmail.com>
2017-09-18 11:54:52 +0100

    test remove dontCacheBustUrlsMatching

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ init();
 if (process.env.NODE_ENV === 'production') {
 	// cache all assets if browser supports serviceworker
 	if ('serviceWorker' in navigator && location.protocol === 'https:') {
-		navigator.serviceWorker.register('/sw-dev.js');
-		//navigator.serviceWorker.register('/service-worker.js');
+		//navigator.serviceWorker.register('/sw-dev.js');
+		navigator.serviceWorker.register('/service-worker.js');
 	}
 
 	// add Google Analytics
