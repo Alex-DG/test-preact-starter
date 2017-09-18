437ab20a1d1ac9474012ae8ec8f58b003a91587a
Alexandre Di Guida <alexandre.diguida@gmail.com>
2017-09-18 14:40:29 +0100

    add start_url

--- src/index.js
+++ src/index.js
@@ -19,12 +19,14 @@ if (process.env.NODE_ENV === 'production') {
 	// }
 
   if ('serviceWorker' in navigator) {
+  console.log('-- SW -- >');
   // Your service-worker.js *must* be located at the top-level directory relative to your site.
   // It won't be able to control pages unless it's located at the same level or higher than them.
   // *Don't* register service worker file in, e.g., a scripts/ sub-directory!
   // See https://github.com/slightlyoff/ServiceWorker/issues/468
   navigator.serviceWorker.register('/service-worker.js').then(function(reg) {
       // updatefound is fired if service-worker.js changes.
+      console.log('[register]');
       reg.onupdatefound = function() {
         // The updatefound event implies that reg.installing is set; see
         // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
