31e0caf9bf1e680ab1e1b57eb7a53851384590f1
Alexandre Di Guida <alexandre.diguida@gmail.com>
2017-09-19 14:24:24 +0100

    test

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === 'production') {
     // It won't be able to control pages unless it's located at the same level or higher than them.
     // *Don't* register service worker file in, e.g., a scripts/ sub-directory!
     // See https://github.com/slightlyoff/ServiceWorker/issues/468
-    navigator.serviceWorker.register('/service-worker.js').then(function(reg) {
+    navigator.serviceWorker.register('/service-worker.js', { cache: 'max-age=0' }).then(function(reg) {
       // updatefound is fired if service-worker.js changes.
       reg.onupdatefound = function() {
         // The updatefound event implies that reg.installing is set; see
