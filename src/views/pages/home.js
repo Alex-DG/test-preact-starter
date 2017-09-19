1253ea8ddb78f3ef93ce939c64d1f60b4a069bb6
Alexandre Di Guida <alexandre.diguida@gmail.com>
2017-09-19 10:48:26 +0100

    test

--- src/views/pages/home.js
+++ src/views/pages/home.js
@@ -6,7 +6,7 @@ export default function (props) {
 	return (
 		<div className="page page__home">
 			<Card>
-				<h1>Bob here</h1>
+				<h1>Bob here or not here</h1>
 				<p>This is the home page.</p>
 
 				<p>You should check out:</p>
